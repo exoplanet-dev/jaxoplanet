1fbcb3a0b033d3dc04ed88ddd2b47333addeaab4
Dan F-M <foreman.mackey@gmail.com>
2022-07-15 11:03:03 +0100

    docstrings

--- src/exo4jax/_src/light_curves.py
+++ src/exo4jax/_src/light_curves.py
@@ -1,15 +1,22 @@
 from functools import partial
-from typing import NamedTuple
+from typing import NamedTuple, Optional
 
 import jax
 import jax.numpy as jnp
 
-from exo4jax._src.orbit_proto import LightCurveOrbit
+from exo4jax._src.proto import LightCurveOrbit
 from exo4jax._src.quad import light_curve
 from exo4jax._src.types import Array
 
 
 class Quad(NamedTuple):
+    """A quadratically limb darkened light curve
+\
+    Args:
+        u1: The first limb darkening coefficient
+        u2: The second limb darkening coefficient
+    """
+
     u1: Array
     u2: Array
 
@@ -40,8 +47,39 @@ class Quad(NamedTuple):
         return jnp.sqrt(jnp.maximum(0, depth * factor))
 
     def light_curve(
-        self, orbit: LightCurveOrbit, t: Array, *, order: int = 10
+        self,
+        orbit: LightCurveOrbit,
+        t: Array,
+        *,
+        texp: Optional[Array] = None,
+        oversample: int = 7,
+        order: int = 10
     ) -> Array:
+        """Get the light curve for an orbit at a set of times
+
+        Args:
+            orbit: An object with a ``get_relative_position`` method that
+                takes a tensor of times and returns a list of Cartesian
+                coordinates of a set of bodies relative to the central source.
+                The first two coordinate dimensions are treated as being in
+                the plane of the sky and the third coordinate is the line of
+                sight with positive values pointing *away* from the observer.
+                For an example, take a look at :class:`orbits.KeplerianOrbit`.
+            t: The times where the light curve should be evaluated.
+            texp (Optional[tensor]): The exposure time of each observation.
+                This can be a scalar or a tensor with the same shape as ``t``.
+                If ``texp`` is provided, ``t`` is assumed to indicate the
+                timestamp at the *middle* of an exposure of length ``texp``.
+            oversample: The number of function evaluations to use when
+                numerically integrating the exposure time.
+            order: The order of the numerical integration scheme used to
+                compute the limb darkening integrals.
+        """
+        del oversample
+        if texp is not None:
+            raise NotImplementedError(
+                "Exposure time integration is not yet implemented"
+            )
         r_star = orbit.central.radius
         x, y, z = orbit.relative_position(t)
         b = jnp.sqrt(x**2 + y**2) / r_star
---
+++ src/exo4jax/light_curves.py
@@ -0,0 +1,1 @@
+from exo4jax._src.light_curves import Quad as Quad
