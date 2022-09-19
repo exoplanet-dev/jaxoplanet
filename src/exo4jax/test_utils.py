dbd647ad79c1e7a7287cddbb0f3de450bcb123b6
Dan F-M <foreman.mackey@gmail.com>
2022-09-19 11:53:58 -0400

    change tolerances

--- src/exo4jax/test_utils.py
+++ src/exo4jax/test_utils.py
@@ -1,9 +1,12 @@
-from functools import partial
-
 import jax
+import jax.numpy as jnp
 import numpy as np
 
-assert_allclose = partial(
-    np.testing.assert_allclose,
-    atol=2e-5 if jax.config.jax_enable_x64 else 2e-4,
-)
+
+def assert_allclose(calculated, expected, *args, **kwargs):
+    dtype = jnp.result_type(calculated, expected)
+    if dtype == jnp.float64:
+        kwargs["atol"] = kwargs.get("atol", 2e-5)
+    else:
+        kwargs["atol"] = kwargs.get("atol", 2e-2)
+    np.testing.assert_allclose(calculated, expected, *args, **kwargs)
