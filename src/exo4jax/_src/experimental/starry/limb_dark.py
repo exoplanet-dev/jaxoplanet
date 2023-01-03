fixing NaNs for out-of-transit gradients for higher order limb dark

src/exo4jax/_src/experimental/starry/limb_dark.py
@@ -29,7 +29,10 @@ def solution_vector(
         b = jnp.abs(b)
         r = jnp.abs(r)
         kappa0, kappa1 = kappas(b, r)
-        P = p_integral(order, l_max, b, r, kappa0)
+        cond = jnp.less(b, 1 + r)
+        b_ = jnp.where(cond, b, 1)
+        P = p_integral(order, l_max, b_, r, kappa0)
+        P = jnp.where(cond, P, 0)
 
         # Hardcoding the Q integral here because Q=0 for n>=2
         lam = 0.5 * jnp.pi - kappa1

src/exo4jax/_src/experimental/starry/solution.py
@@ -61,7 +61,7 @@ def q_integral(l_max: int, lam: Array) -> Array:
         return comp
 
     U = []
-    for l in range(l_max + 1):
+    for l in range(l_max + 1):  # noqa
         for m in range(-l, l + 1):
             if l == 1 and m == 0:
                 U.append((np.pi + 2 * lam) / 3)
@@ -110,7 +110,7 @@ def p_integral(
     ind = []
     arg = []
     n = 0
-    for l in range(l_max + 1):
+    for l in range(l_max + 1):  # noqa
         fa3 = (2 * r) ** (l - 1) * f0
         for m in range(-l, l + 1):
             mu = l - m