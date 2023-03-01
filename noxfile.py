d7116951c514718994e0eb6ffbb0b4ea093070b2
Dan Foreman-Mackey <foreman.mackey@gmail.com>
2023-03-01 10:13:20 -0500

    Switching to poetry for infra (#15)

--- noxfile.py
+++ noxfile.py
@@ -18,7 +18,7 @@ def test(session, x64):
 
 @nox.session(python=ALL_PYTHON_VS)
 def comparison(session):
-    session.install(".[test,comparison]")
+    session.install(".[test,comparison]", "numpy<1.22")
     session.run(
         "pytest",
         "-n",
