d42637182d1acf42a27f59e82de2d1a1452097ec
Dan F-M <foreman.mackey@gmail.com>
2023-03-11 08:11:25 -0500

    add light curve object

--- src/jaxoplanet/_src/light_curves.py
+++ src/jaxoplanet/_src/light_curves.py
@@ -3,9 +3,65 @@ from typing import NamedTuple, Optional
 
 import jax.numpy as jnp
 
-from jaxoplanet._src.core.quad import light_curve
+from jaxoplanet._src.core.limb_dark import light_curve as limb_dark_light_curve
+from jaxoplanet._src.core.quad import light_curve as quad_light_curve
 from jaxoplanet._src.proto import LightCurveOrbit
 from jaxoplanet._src.types import Array
+
+
+class LimbDarkLightCurve(NamedTuple):
+    u: Array
+
+    @classmethod
+    def init(cls, u: Array, *rest: Array) -> "LimbDarkLightCurve":
+        if rest:
+            u = jnp.stack((u, *rest))
+        return cls(u=u)
+
+    def light_curve(
+        self,
+        orbit: LightCurveOrbit,
+        t: Array,
+        *,
+        texp: Optional[Array] = None,
+        oversample: int = 7,
+        order: int = 10,
+    ) -> Array:
+        """Get the light curve for an orbit at a set of times
+
+        Args:
+            orbit: An object with a ``get_relative_position`` method that
+                takes a tensor of times and returns a list of Cartesian
+                coordinates of a set of bodies relative to the central source.
+                The first two coordinate dimensions are treated as being in
+                the plane of the sky and the third coordinate is the line of
+                sight with positive values pointing *away* from the observer.
+                For an example, take a look at :class:`orbits.KeplerianOrbit`.
+            t: The times where the light curve should be evaluated.
+            texp (Optional[tensor]): The exposure time of each observation.
+                This can be a scalar or a tensor with the same shape as ``t``.
+                If ``texp`` is provided, ``t`` is assumed to indicate the
+                timestamp at the *middle* of an exposure of length ``texp``.
+            oversample: The number of function evaluations to use when
+                numerically integrating the exposure time.
+        """
+        del oversample
+        if texp is not None:
+            raise NotImplementedError(
+                "Exposure time integration is not yet implemented"
+            )
+        r_star = orbit.central_radius
+        x, y, z = orbit.relative_position(t)
+        b = jnp.sqrt(x**2 + y**2)
+        r = orbit.radius / r_star
+        lc_func = partial(limb_dark_light_curve, self.u, order=order)
+        if orbit.shape == ():
+            b /= r_star
+            lc = lc_func(b, r)
+        else:
+            b /= r_star[..., None]
+            lc = jnp.vectorize(lc_func, signature="(k),()->(k)")(b, r)
+        return jnp.where(z > 0, lc, 0)
 
 
 class QuadLightCurve(NamedTuple):
@@ -71,8 +127,6 @@ class QuadLightCurve(NamedTuple):
                 timestamp at the *middle* of an exposure of length ``texp``.
             oversample: The number of function evaluations to use when
                 numerically integrating the exposure time.
-            order: The order of the numerical integration scheme used to
-                compute the limb darkening integrals.
         """
         del oversample
         if texp is not None:
@@ -83,7 +137,7 @@ class QuadLightCurve(NamedTuple):
         x, y, z = orbit.relative_position(t)
         b = jnp.sqrt(x**2 + y**2)
         r = orbit.radius / r_star
-        lc_func = partial(light_curve, self.u1, self.u2, order=order)
+        lc_func = partial(quad_light_curve, self.u1, self.u2, order=order)
         if orbit.shape == ():
             b /= r_star
             lc = lc_func(b, r)
