f68fc0fa769ef3bc593898b15f8145d73e5e1e5b
Dan Foreman-Mackey <foreman.mackey@gmail.com>
2023-03-28 15:51:11 -0400

    Adding documentation infrastructure (#31)
    * Starting to implement documentation
    
    * adding dependencies properly
    
    * version specifications
    
    * lowercase
    
    * install jax[cpu] explicity for tests
    
    * jaxlib as optional dep
    
    * docs to install JAX
    
    * install jaxlib on rtds
    
    * adding nox session to build docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,45 +1,54 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import jaxoplanet
 
-from pkg_resources import DistributionNotFound, get_distribution
-
-try:
-    __version__ = get_distribution("jaxoplanet").version
-except DistributionNotFound:
-    __version__ = "dev"
-
+language = "en"
+master_doc = "index"
 
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
     "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_design",
     "myst_nb",
+    "IPython.sphinxext.ipython_console_highlighting",
 ]
-autodoc_mock_imports = []
 
+myst_enable_extensions = ["dollarmath", "colon_fence"]
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".ipynb": "myst-nb",
+}
+templates_path = ["_templates"]
+
+# General information about the project.
 project = "jaxoplanet"
-copyright = "2021 Dan Foreman-Mackey"
-version = __version__
-release = __version__
+copyright = "2021-2023 Simons Foundation, Inc."
+version = jaxoplanet.__version__
+release = jaxoplanet.__version__
 
 exclude_patterns = ["_build"]
-html_static_path = ["_static"]
 html_theme = "sphinx_book_theme"
-html_title = "exoplanet-jax"
+html_title = "jaxoplanet documentation"
+html_logo = "_static/logo.png"
+html_favicon = "_static/favicon.png"
+html_static_path = ["_static"]
 html_show_sourcelink = False
-html_baseurl = "https://jaxoplanet.readthedocs.io/en/latest/"
 html_theme_options = {
     "path_to_docs": "docs",
     "repository_url": "https://github.com/exoplanet-dev/jaxoplanet",
     "repository_branch": "main",
     "launch_buttons": {
         "binderhub_url": "https://mybinder.org",
-        "colab_url": "https://colab.research.google.com",
         "notebook_interface": "jupyterlab",
+        "colab_url": "https://colab.research.google.com/",
     },
     "use_edit_page_button": True,
     "use_issues_button": True,
     "use_repository_button": True,
     "use_download_button": True,
+    "use_sidenotes": True,
 }
+nb_execution_mode = "auto"
+nb_execution_excludepatterns = []
+nb_execution_timeout = -1
