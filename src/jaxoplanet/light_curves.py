90723c352603507f2798c29f85a800969326a15b
Soichiro Hattori <soichiro.hattori@gmail.com>
2023-08-04 15:00:08 -0400

    changes made for implementing a working version of exposure time integration

--- src/jaxoplanet/light_curves.py
+++ src/jaxoplanet/light_curves.py
@@ -26,7 +26,7 @@ class LimbDarkLightCurve(NamedTuple):
         *,
         texp: Optional[Array] = None,
         oversample: Optional[int] = 7,
-        stencil_order: Optional[int] = 0,
+        texp_order: Optional[int] = 0,
         order: Optional[int] = 10,
     ) -> Array:
         """Get the light curve for an orbit at a set of times
@@ -46,78 +46,55 @@ class LimbDarkLightCurve(NamedTuple):
                 timestamp at the *middle* of an exposure of length ``texp``.
             oversample: The number of function evaluations to use when
                 numerically integrating the exposure time.
-            stencil_order (Optional[int]): The order of the numerical integration
+            texp_order (Optional[int]): The order of the numerical integration
                 scheme. This must be one of the following: ``0`` for a
                 centered Riemann sum (equivalent to the "resampling" procedure
                 suggested by Kipping 2010), ``1`` for the trapezoid rule, or
                 ``2`` for Simpson's rule.
+            order (Optional[int]): ADD DOCUMENTATION. I know this is related
+                to the order of the legendre polynomial used in the core component
         """
 
-        r_transit = jnp.atleast_1d(orbit.radius)
-        r_transit = jnp.reshape(r_transit, r_transit.shape)
         t = jnp.atleast_1d(t)
-
-        print(r_transit.shape)
 
         # Handle exposure time integration
         if texp is None:
             tgrid = t
-            rgrid = jnp.reshape(
-                r_transit, newshape=(1,) * tgrid.ndim + r_transit.shape
-            )  # left shape padding
-            rgrid += jnp.reshape(
-                jnp.zeros_like(tgrid), newshape=tgrid.shape + (1,) * r_transit.ndim
-            )  # right shape padding
-
         else:
-            assert texp.shape == t.shape, "texp must have the same shape as t"
+            assert texp.shape == t.shape, "texp must have the same shape as t (change?)"
 
             # Ensure oversample is an odd number
             oversample = int(oversample)
             oversample += 1 - oversample % 2
-            stencil = jnp.ones(oversample, dtype=int)
+            stencil = jnp.ones(oversample)
 
             # Construct exposure time integration stencil
-            if stencil_order == 0:
+            if texp_order == 0:
                 dt = jnp.linspace(-0.5, 0.5, 2 * oversample + 1)[1:-1:2]
-            elif stencil_order == 1:
+            elif texp_order == 1:
                 dt = jnp.linspace(-0.5, 0.5, oversample)
-                stencil[1:-1] = 2
-            elif stencil_order == 2:
+                stencil = 2 * stencil
+                stencil = stencil.at[0].set(1)
+                stencil = stencil.at[-1].set(1)
+            elif texp_order == 2:
                 dt = jnp.linspace(-0.5, 0.5, oversample)
-                stencil[1:-1:2] = 4
-                stencil[2:-1:2] = 2
+                stencil = stencil.at[1:-1:2].set(4)
+                stencil = stencil.at[2:-1:2].set(2)
             else:
                 raise ValueError("order must be 0, 1, or 2")
-            # stencil /= jnp.sum(stencil)
-            print(f"stencil = {stencil}")
-            print(f"stencil.shape = {stencil.shape}")
+            stencil /= jnp.sum(stencil)
 
             if texp.ndim == 0:  # Unnecessary since I check the shapes above?
                 dt = texp * dt
             else:
-                dt = jnp.reshape(texp, newshape=texp.shape + (1,)) * dt
-            tgrid = jnp.reshape(t, newshape=t.shape + (1,)) + dt
-            rgrid = jnp.reshape(
-                r_transit, newshape=(1,) * tgrid.ndim + r_transit.shape
-            )  # left shape padding
-            rgrid += jnp.reshape(
-                jnp.zeros_like(tgrid), newshape=tgrid.shape + (1,)
-            )  # right shape padding
-
-        print(f"tgrid.shape = {tgrid.shape}")
-        print(f"rgrid.shape = {rgrid.shape}")
-        # print(f"stenil.shape = {stencil.shape}")
+                dt = jnp.outer(texp, dt)
+            tgrid = (t.reshape(t.size, 1) + dt).flatten()
 
         # Evaluate the coordinates of the transiting body
-        # Correct tgrid and rgrid such that light curve has correct shape of (1, 1000)
-
         x, y, z = orbit.relative_position(tgrid)
         b = jnp.sqrt(x**2 + y**2)
-        # b = jnp.reshape(b, newshape=rgrid.shape)
         r_star = orbit.central_radius
-        # r = rgrid / r_star
-        r = r_transit / r_star
+        r = orbit.radius / r_star
         lc_func = partial(light_curve, self.u, order=order)
         if orbit.shape == ():
             b /= r_star
@@ -126,30 +103,11 @@ class LimbDarkLightCurve(NamedTuple):
             b /= r_star[..., None]
             lc = jnp.vectorize(lc_func, signature="(k),()->(k)")(b, r)
         lc = jnp.where(z > 0, lc, 0)
-        lc = lc[0]
 
         # Integrate over exposure time
         if texp is not None:
-            stencil = jnp.reshape(
-                stencil, newshape=(1,) * t.ndim + stencil.shape
-            )  # left shape padding
-            # stencil = jnp.reshape(
-            # stencil, newshape=stencil.shape + (1,)
-            # )  # right shape padding
-            summed_lc = jnp.sum(lc * stencil, axis=t.ndim)
-            return lc, summed_lc, stencil, x, y, z
-        else:
-            return lc, x, y, z
-
-        # r_star = orbit.central_radius
-        # x, y, z = orbit.relative_position(t)
-        # b = jnp.sqrt(x**2 + y**2)
-        # r = orbit.radius / r_star
-        # lc_func = partial(light_curve, self.u, order=order)
-        # if orbit.shape == ():
-        #     b /= r_star
-        #     lc = lc_func(b, r)
-        # else:
-        #     b /= r_star[..., None]
-        #     lc = jnp.vectorize(lc_func, signature="(k),()->(k)")(b, r)
-        # return jnp.where(z > 0, lc, 0)
+            lc = lc[0]
+            lc = jnp.reshape(lc, newshape=(t.size, oversample))
+            stencil = jnp.reshape(stencil, newshape=stencil.shape + (1,))
+            lc = (lc @ stencil).T
+        return lc
