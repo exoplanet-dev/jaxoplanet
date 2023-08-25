0a0a1f803891056d12666db30fecf2fb2dbbd23b
Dan F-M <foreman.mackey@gmail.com>
2023-08-25 14:00:06 -0400

    not hashable

--- tests/keplerian_test.py
+++ tests/keplerian_test.py
@@ -9,4 +9,4 @@ def test_keplerian_central_shape():
 
 def test_casting_dtype():
     orbit = orbits.KeplerianBody.init(period=1)
-    assert orbit.period.dtype in {jnp.float32, jnp.float64}
+    assert orbit.period.dtype in (jnp.float32, jnp.float64)
