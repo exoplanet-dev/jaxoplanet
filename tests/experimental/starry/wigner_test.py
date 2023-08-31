4b0b834b52fc710d333bade08bcff348ce4c6432
Dan F-M <foreman.mackey@gmail.com>
2023-08-30 20:56:47 -0400

    don't test with square matrices

--- tests/experimental/starry/wigner_test.py
+++ tests/experimental/starry/wigner_test.py
@@ -12,7 +12,7 @@ def test_matches_dotR(l_max, u):
     theta = 0.1
     np.random.seed(l_max)
     n_max = l_max**2 + 2 * l_max + 1
-    M = np.random.rand(n_max, n_max)
+    M = np.random.rand(5, n_max)
     expected = dotR(l_max, u)(M, theta)
     calc = dot_rotation_matrix(l_max, *u, theta)(M)
     assert_allclose(calc, expected)
@@ -26,7 +26,7 @@ def test_compare_starry_rot_rotation(l_max, u):
     theta = 0.1
     np.random.seed(l_max)
     n_max = l_max**2 + 2 * l_max + 1
-    M = np.random.rand(n_max, n_max)
+    M = np.random.rand(5, n_max)
     m = starry._core.core.OpsYlm(l_max, 0, 0, 1)
     expected = m.dotR(M, *u, theta)
     calc = dot_rotation_matrix(l_max, *u, theta)(M)
