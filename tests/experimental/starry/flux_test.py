80aa4cbc76658f81df43055ea7065a86dda749a8
lgrcia <lionel_garcia@live.fr>
2023-11-21 10:23:02 -0500

    chore: adding flux tests

--- tests/experimental/starry/flux_test.py
+++ tests/experimental/starry/flux_test.py
@@ -2,10 +2,12 @@ import pytest
 import numpy as np
 import jax.numpy as jnp
 from jaxoplanet.experimental.starry.flux import flux
+from jaxoplanet.test_utils import assert_allclose
 
 
 @pytest.mark.parametrize("deg", [2, 5, 10])
-def test_compare_starry(deg):
+@pytest.mark.parametrize("u", [[], [0.1], [0.2, 0.1]])
+def test_compare_starry(deg, u):
     starry = pytest.importorskip("starry")
 
     # map
@@ -22,13 +24,14 @@ def test_compare_starry(deg):
     xo = 0.0
 
     # starry
-    ms = starry.Map(ydeg=deg, udeg=0, inc=np.rad2deg(inc))
+    ms = starry.Map(ydeg=deg, udeg=len(u), inc=np.rad2deg(inc))
     ms[:, :] = y
+    ms[1 : (len(u) + 1)] = u
     expected = ms.flux(xo=xo, yo=yo, ro=ro, zo=zo)
 
     # jaxoplanet
     calc = jnp.vectorize(
-        flux, signature="(),(),(),(),(),(),(),(n),(),()->()", excluded={0}
-    )(deg, 0.0, xo, yo, zo, ro, inc, 0.0, y, 0, 0.0)
+        flux, signature="(),(),(),(),(),(),(),(n),()->()", excluded={0, 9}
+    )(deg, 0.0, xo, yo, zo, ro, inc, 0.0, y, u, 0.0)
 
-    np.assert_allclose(calc, expected)
+    assert_allclose(calc, expected)
