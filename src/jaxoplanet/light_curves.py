9f6b2716bef07834f69569607d3dd0c236449253
Soichiro Hattori <soichiro.hattori@gmail.com>
2024-02-22 18:35:26 -0500

    backwards-compatible light_curve method and adding integrated_light_curve method

--- src/jaxoplanet/light_curves.py
+++ src/jaxoplanet/light_curves.py
@@ -1,14 +1,19 @@
 from functools import partial
+from typing import Optional, TypeVar
 
 import equinox as eqx
+import jax
 import jax.numpy as jnp
 import jpu.numpy as jnpu
 
 from jaxoplanet import units
 from jaxoplanet.core.limb_dark import light_curve
+from jaxoplanet.exposure_time import _LightCurveFunc, integrate_exposure_time
 from jaxoplanet.proto import LightCurveOrbit
 from jaxoplanet.types import Array, Quantity
 from jaxoplanet.units import unit_registry as ureg
+
+T = TypeVar("T", Array, Quantity, covariant=True)
 
 
 class LimbDarkLightCurve(eqx.Module):
@@ -26,7 +31,7 @@ class LimbDarkLightCurve(eqx.Module):
         orbit: LightCurveOrbit,
         time: Quantity,
         *,
-        order: int = 10,
+        ld_order: int = 10,
     ) -> Array:
         """Get the light curve for an orbit at a set of times
 
@@ -39,7 +44,7 @@ class LimbDarkLightCurve(eqx.Module):
                 sight with positive values pointing *away* from the observer.
                 For an example, take a look at :class:`orbits.keplerian`.
             time: The time where the light curve should be evaluated.
-            order: The quadrature order passed to the
+            ld_order: The quadrature order passed to the
                 ``scipy.special.roots_legendre`` function in ``core.limb_dark.py``
                 which implements Gauss-Legendre quadrature. Defaults to 10.
         """
@@ -57,13 +62,59 @@ class LimbDarkLightCurve(eqx.Module):
         x, y, z = orbit.relative_position(time)
         b = jnpu.sqrt(x**2 + y**2) / r_star  # type: ignore
         r = orbit.radius / r_star
-        lc_func = partial(light_curve, self.u, order=order)
+        lc_func = partial(light_curve, self.u, order=ld_order)
         lc: Array = lc_func(b.magnitude, r.magnitude)
         lc = jnp.where(z > 0, lc, 0)
 
         return lc
 
+    def _light_curve(
+        self, orbit: LightCurveOrbit, time: Quantity, *, ld_order: int = 10
+    ) -> Quantity:
+        return self(orbit, time, ld_order=ld_order)
+
     def light_curve(
-        self, orbit: LightCurveOrbit, time: Quantity, *, order: int = 10
+        self, orbit: LightCurveOrbit, time: Quantity, *, ld_order: int = 10
     ) -> Array:
-        return self(orbit, time, order=order)
+        def _lc_func(time: time) -> _LightCurveFunc[T]:
+            return self._light_curve(orbit=orbit, time=time, ld_order=ld_order)
+
+        return jax.vmap(_lc_func, in_axes=(0))(time)
+
+    def _integrated_light_curve(
+        self,
+        orbit: LightCurveOrbit,
+        time: Quantity,
+        *,
+        ld_order: int = 10,
+        exposure_time: Optional[Quantity] = None,
+        order: int = 0,
+        num_samples: int = 7,
+    ) -> _LightCurveFunc[T]:
+        def _partial_lc_func(time):
+            return self._light_curve(orbit=orbit, time=time, ld_order=ld_order)
+
+        _integrated_lc_func = integrate_exposure_time(
+            _partial_lc_func, exposure_time=exposure_time, num_samples=num_samples
+        )
+        return _integrated_lc_func
+
+    def integrated_light_curve(
+        self,
+        orbit: LightCurveOrbit,
+        time: Quantity,
+        *,
+        ld_order: int = 10,
+        exposure_time: Optional[Quantity] = None,
+        order: int = 0,
+        num_samples: int = 7,
+    ) -> Array:
+        _integrated_lc_func = self._integrated_light_curve(
+            orbit=orbit,
+            time=time,
+            ld_order=ld_order,
+            exposure_time=exposure_time,
+            order=order,
+            num_samples=num_samples,
+        )
+        return jax.vmap(_integrated_lc_func, in_axes=(0))(time)
