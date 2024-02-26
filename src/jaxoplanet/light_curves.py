63c0a8aa6550825b0a5fd32ec7eaeeecfe5f1b41
Dan Foreman-Mackey <foreman.mackey@gmail.com>
2024-02-26 12:00:45 -0500

    Experimenting with a different interface for exposure time (#129)
    * Experimenting with a different interface for exposure time
    
    * Using linear_util
    
    * Allowing generic args and kwargs in exposure time integration
    
    * Fixing logic for limb_dark light curve
    
    * backwards-compatible light_curve method and adding integrated_light_curve method
    
    * staging
    
    * updating after chat with @soichiro-hattori
    
    * typos
    
    * fixing tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * correct shape
    
    * correct shape, now?
    
    * transit tutorial syntax
    
    ---------
    
    Co-authored-by: Soichiro Hattori <soichiro.hattori@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/jaxoplanet/light_curves.py
+++ src/jaxoplanet/light_curves.py
@@ -1,127 +1,47 @@
 from functools import partial
-from typing import Optional
+from typing import Callable
 
-import equinox as eqx
 import jax.numpy as jnp
 import jpu.numpy as jnpu
 
 from jaxoplanet import units
-from jaxoplanet.core.limb_dark import light_curve
+from jaxoplanet.core.limb_dark import light_curve as _limb_dark_light_curve
 from jaxoplanet.proto import LightCurveOrbit
 from jaxoplanet.types import Array, Quantity
 from jaxoplanet.units import unit_registry as ureg
 
 
-class LimbDarkLightCurve(eqx.Module):
-    """A light curve"""
+def limb_dark_light_curve(
+    orbit: LightCurveOrbit, *u: Array, order: int = 10
+) -> Callable[[Quantity], Array]:
+    if u:
+        ld_u = jnp.concatenate([jnp.atleast_1d(jnp.asarray(u_)) for u_ in u], axis=0)
+    else:
+        ld_u = jnp.array([])
 
-    u: Array
-
-    def __init__(self, *u: Array):
-        if u:
-            self.u = jnp.concatenate([jnp.atleast_1d(u0) for u0 in u], axis=0)
-        else:
-            self.u = jnp.array([])
-
-    @units.quantity_input(t=ureg.d, texp=ureg.s)
-    def light_curve(
-        self,
-        orbit: LightCurveOrbit,
-        t: Quantity,
-        *,
-        texp: Optional[Quantity] = None,
-        oversample: Optional[int] = 7,
-        texp_order: Optional[int] = 0,
-        limbdark_order: Optional[int] = 10,
-    ) -> Array:
-        """Get the light curve for an orbit at a set of times
-
-        Args:
-            orbit: An object with a ``relative_position`` method that
-                takes a tensor of times and returns a list of Cartesian
-                coordinates of a set of bodies relative to the central source.
-                The first two coordinate dimensions are treated as being in
-                the plane of the sky and the third coordinate is the line of
-                sight with positive values pointing *away* from the observer.
-                For an example, take a look at :class:`orbits.KeplerianOrbit`.
-            t: The times where the light curve should be evaluated.
-            texp (Optional[Array]): The exposure time of each observation.
-                This can be an Array with the same shape as ``t``.
-                If ``texp`` is provided, ``t`` is assumed to indicate the
-                timestamp at the *middle* of an exposure of length ``texp``.
-            oversample: The number of function evaluations to use when
-                numerically integrating the exposure time. Defaults to 7.
-            texp_order (Optional[int]): The order of the numerical integration
-                scheme. This must be one of the following: ``0`` for a
-                centered Riemann sum (equivalent to the "resampling" procedure
-                suggested by Kipping 2010), ``1`` for the trapezoid rule, or
-                ``2`` for Simpson's rule. Has no effect if ``texp`` is not set.
-                Defaults to 0.
-            limbdark_order (Optional[int]): The quadrature order passed to the
-                ``scipy.special.roots_legendre`` function in ``core.limb_dark.py``
-                which implements Gauss-Legendre quadrature. Defaults to 10.
-        """
-
-        t = jnpu.atleast_1d(t)
-
-        # Handle exposure time integration
-        if texp is None:
-            tgrid = t
-        else:
-            if (
-                jnp.ndim(texp) == 0
-            ):  # If a scalar is passed, automatically broadcast to the shape of t
-                texp = jnpu.broadcast_to(texp, jnp.shape(t))
-            assert texp.shape == t.shape
-
-            # Ensure oversample is an odd number
-            oversample = int(oversample)
-            oversample += 1 - oversample % 2
-            stencil = jnp.ones(oversample)
-
-            # Construct exposure time integration stencil
-            if texp_order == 0:
-                dt = jnp.linspace(-0.5, 0.5, 2 * oversample + 1)[1:-1:2]
-            elif texp_order == 1:
-                dt = jnp.linspace(-0.5, 0.5, oversample)
-                stencil = 2 * stencil
-                stencil = stencil.at[0].set(1)
-                stencil = stencil.at[-1].set(1)
-            elif texp_order == 2:
-                dt = jnp.linspace(-0.5, 0.5, oversample)
-                stencil = stencil.at[1:-1:2].set(4)
-                stencil = stencil.at[2:-1:2].set(2)
-            else:
-                raise ValueError("order must be 0, 1, or 2")
-            stencil /= jnp.sum(stencil)
-
-            if jnp.ndim(texp) == 0:  # Unnecessary since I check the shapes above?
-                dt = texp * dt
-            else:
-                dt = jnpu.outer(texp, dt)
-            tgrid = (jnpu.reshape(t, newshape=(t.size, 1)) + dt).flatten()
+    @units.quantity_input(time=ureg.d)
+    def light_curve_impl(time: Quantity) -> Array:
+        if jnpu.ndim(time) != 0:
+            raise ValueError(
+                "The time passed to 'light_curve' has shape "
+                f"{jnpu.shape(time)}, but a scalar was expected; "
+                "To use exposure time integration for an array of times, "
+                "manually 'vmap' or 'vectorize' the function"
+            )
 
         # Evaluate the coordinates of the transiting body
-        x, y, z = orbit.relative_position(tgrid)
-        b = jnpu.sqrt(x**2 + y**2)
         r_star = orbit.central_radius
+        x, y, z = orbit.relative_position(time)
+
+        b = jnpu.sqrt(x**2 + y**2) / r_star
+        assert b.units == ureg.dimensionless
         r = orbit.radius / r_star
-        lc_func = partial(light_curve, self.u, order=limbdark_order)
-        if orbit.shape == ():
-            b /= r_star
-            lc: Array = lc_func(b.magnitude, r.magnitude)
-        else:
-            b /= r_star[..., None]
-            lc = jnp.vectorize(lc_func, signature="(k),()->(k)")(
-                b.magnitude, r.magnitude
-            )
+        assert r.units == ureg.dimensionless
+
+        lc_func = partial(_limb_dark_light_curve, ld_u, order=order)
+        lc = lc_func(b.magnitude, r.magnitude)
         lc = jnp.where(z > 0, lc, 0)
 
-        # Integrate over exposure time
-        if texp is not None:
-            if orbit.shape == ():
-                lc = jnp.reshape(lc, newshape=(t.size, oversample))
-            else:
-                lc = jnp.reshape(lc, newshape=(orbit.shape[0], t.size, oversample))
-            lc = jnp.dot(lc, stencil)
         return lc
+
+    return light_curve_impl
--- tests/light_curves_test.py
+++ tests/light_curves_test.py
@@ -1,7 +1,8 @@
+import jax
 import jax.numpy as jnp
 
-from jaxoplanet.light_curves import LimbDarkLightCurve
-from jaxoplanet.orbits.keplerian import Central, System
+from jaxoplanet.light_curves import limb_dark_light_curve
+from jaxoplanet.orbits.keplerian import System
 
 
 def test_light_curve():
@@ -21,21 +22,5 @@ def test_light_curve():
 
     # Compute a limb-darkened light curve using jaxoplanet
     t = jnp.linspace(-0.3, 0.3, 1000)
-
-    lc = LimbDarkLightCurve(params["u"]).light_curve(orbit, t=t)
-    assert lc.shape == (1,) + t.shape
-
-
-def test_multiplanetary():
-    star = Central(mass=5, radius=2)
-    system = System(star).add_body(radius=0.05, period=1).add_body(radius=0.2, period=2)
-
-    t = jnp.linspace(-5, 5, 500)
-    lc = LimbDarkLightCurve().light_curve(system, t)
-    lc_texp_scalar = LimbDarkLightCurve().light_curve(system, t, texp=10)
-    lc_texp_array = LimbDarkLightCurve().light_curve(
-        system, t, texp=jnp.broadcast_to(10, t.size)
-    )
-    assert lc.shape == (system.shape[0], t.size)
-    assert lc_texp_scalar.shape == (system.shape[0], t.size)
-    assert lc_texp_array.shape == (system.shape[0], t.size)
+    lc = jax.vmap(limb_dark_light_curve(orbit, params["u"]))(t)
+    assert lc.shape == t.shape + (1,)
