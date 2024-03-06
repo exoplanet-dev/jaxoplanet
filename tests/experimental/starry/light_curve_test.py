f782187608227654955970334031e381e9b8c6f3
Soichiro Hattori <soichiro.hattori@gmail.com>
2024-03-05 22:51:45 -0500

    update test

--- tests/experimental/starry/light_curve_test.py
+++ tests/experimental/starry/light_curve_test.py
@@ -130,7 +130,7 @@ def test_compare_starry_system(keplerian_system):
 
     # jaxoplanet system
     central = keplerian_system.central
-    central_map = keplerian_system.surface_map
+    central_map = keplerian_system.central_surface_map
     body = keplerian_system.bodies[0]
     body_map = keplerian_system.bodies_surface_maps[0]
 
