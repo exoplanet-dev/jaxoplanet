fix: nan value for float32 rotation

src/jaxoplanet/experimental/starry/rotation.py
@@ -44,9 +44,7 @@ def dot_rotation_matrix(ydeg, x, y, z, theta):
     if jnp.shape(theta) != ():
         raise ValueError(f"theta must be a scalar; got {jnp.shape(theta)}")
 
-    rotation_matrices = compute_rotation_matrices_s2fft(
-        ydeg, x, y, z, theta, homogeneous=False
-    )
+    rotation_matrices = compute_rotation_matrices_s2fft(ydeg, x, y, z, theta)
     n_max = ydeg**2 + 2 * ydeg + 1
 
     @jax.jit
@@ -73,7 +71,7 @@ def dot_rotation_matrix(ydeg, x, y, z, theta):
 
 
 def right_project_axis_angle(inc, obl, theta, theta_z):
-    f = 0.5 * math.sqrt(2)
+    f = 0.5 * jnp.sqrt(2)
     si = jnp.sin(inc / 2)
     ci = jnp.cos(inc / 2)
     sp = jnp.sin(0.5 * (obl + theta + theta_z))
@@ -87,14 +85,18 @@ def right_project_axis_angle(inc, obl, theta, theta_z):
     arg = si * cm + ci * cp
     denominator = jnp.sqrt(1 - 0.5 * arg**2)
 
-    angle = 2 * jnp.arccos(f * arg)
+    farg = f * arg
+    zero_angle = jnp.allclose(farg, 1.0, atol=get_dtype_eps(farg))
+    angle = jnp.where(zero_angle, 0.0, 2 * jnp.arccos(farg))
+
+    non_zero_angle = jnp.logical_not(zero_angle)
 
     # this is mostly useful for the float32 case, where
     # (1 - 0.5 * arg**2) in denominator can be negative due to numerical error
     positive_arg = arg**2 < 2.0
-    axis_x = jnp.where(positive_arg, numerator1 / denominator, 1.0)
-    axis_y = jnp.where(positive_arg, numerator2 / denominator, 0.0)
-    axis_z = jnp.where(positive_arg, numerator3 / denominator, 0.0)
+    axis_x = jnp.where(positive_arg & non_zero_angle, numerator1 / denominator, 1.0)
+    axis_y = jnp.where(positive_arg & non_zero_angle, numerator2 / denominator, 0.0)
+    axis_z = jnp.where(positive_arg & non_zero_angle, numerator3 / denominator, 0.0)
 
     return axis_x, axis_y, axis_z, angle
 