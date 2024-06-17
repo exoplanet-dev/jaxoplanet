7e5882c7b287b7d592e511ef7d91af458964d9d6
lgrcia <20612771+lgrcia@users.noreply.github.com>
2024-06-17 10:11:57 -0400

    fix: amplitude as jax array

--- src/jaxoplanet/experimental/starry/surface.py
+++ src/jaxoplanet/experimental/starry/surface.py
@@ -89,7 +89,7 @@ class Surface(eqx.Module):
             y = Ylm()
 
         if normalize:
-            amplitude = float(y[(0, 0)])
+            amplitude = jnp.array(y[(0, 0)], float)
             y = Ylm(data=y.data).normalize()
 
         self.y = y
