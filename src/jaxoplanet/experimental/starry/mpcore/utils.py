ce74b3af5218b4eca8e71092f95cf1270cc90d1c
lgrcia <20612771+lgrcia@users.noreply.github.com>
2024-08-01 11:36:41 -0400

    feat: better diff function to compare any matrix type (numpy vs. mpmath)

--- src/jaxoplanet/experimental/starry/mpcore/utils.py
+++ src/jaxoplanet/experimental/starry/mpcore/utils.py
@@ -1,4 +1,5 @@
 from jaxoplanet.experimental.starry.mpcore import mp
+import mpmath
 import numpy as np
 
 
@@ -16,8 +17,29 @@ def to_mp(x):
     return mp.matrix(x.tolist())
 
 
-def diff_mp(mp_matrix, np_array):
-    return to_numpy(to_mp(np_array) - mp.matrix(mp_matrix))
+def diff_mp(M1: np.ndarray | mpmath.matrix, M2: np.ndarray | mpmath.matrix):
+    """Returns M1 - M2, at arbitrary precision and casted to numpy.float64.
+
+    This function allows comparison of matrices at arbitrary precision if at least one is
+    a mpmath matrix.
+
+    Args:
+        M1 (np.ndarray or mpmath.matrix): matrix
+        M2 (np.ndarray or mpmath.matrix): matrix
+
+    Returns:
+        np.ndarray:
+            difference between M1 and M2, casted to numpy.float64
+    """
+    if isinstance(M1, np.ndarray) and isinstance(M2, np.ndarray):
+        d = M1 - M2
+    else:
+        if (M1, np.ndarray):
+            _M1 = to_mp(M1)
+        if (M2, np.ndarray):
+            _M2 = to_mp(M2)
+        d = to_numpy(_M1 - _M2).astype(np.float64)
+    return d
 
 
 def kron_delta(m, n):
