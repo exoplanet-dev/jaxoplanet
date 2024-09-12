66601de1221225fa53cef30071ec1902003a736d
Lionel Garcia <lionel_garcia@live.fr>
2024-09-12 11:45:28 -0400

    feat: add a multi-precision module for testing (#210)
    * feat: add a multi-precision module for testing
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix: linter errors
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * feat: a closed form multi-precision A2 (way faster than inverting A2_inv)
    
    * fix: mp diff
    
    * feat: caching big matrices
    
    * fix: avoid adding zero elements
    
    * test: multi-precision rotation matrices, basis,solution and flux. At low lmax for now
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix: top-level imports error
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * feat: cached matrices as kwarg
    
    * feat: add gmpy2 for faster multi-precision arithmetic
    
    * fix: slightly change the caching per module
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix: cache added for R
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix: caching or R
    
    * fix: remove precision plots from tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ src/jaxoplanet/experimental/starry/multiprecision/flux.py
@@ -0,0 +1,67 @@
+from collections import defaultdict
+
+from jaxoplanet.experimental.starry.multiprecision import mp
+from jaxoplanet.experimental.starry.multiprecision.basis import A1, A2
+from jaxoplanet.experimental.starry.multiprecision.rotation import (
+    dot_rotation_matrix,
+    dot_rz,
+    get_R,
+)
+from jaxoplanet.experimental.starry.multiprecision.solution import get_sT, rT
+
+
+def flux_function(l_max, inc, obl, cache=None):
+    if cache is None:
+        cache = defaultdict(
+            lambda: {
+                "R_obl": {},
+                "R_inc": {},
+            }
+        )
+
+    _rT = rT(l_max)
+    _A1 = A1(l_max, cache=cache)
+    _A2 = A2(l_max, cache=cache)
+
+    R_px = get_R("R_px", l_max, cache=cache)
+    R_mx = get_R("R_mx", l_max, cache=cache)
+    R_obl = get_R("R_obl", l_max, obl=obl, inc=inc, cache=cache)
+    R_inc = get_R("R_inc", l_max, obl=obl, inc=inc, cache=cache)
+
+    def rotate_y(y, phi):
+        y_rotated = dot_rotation_matrix(l_max, None, None, R_px)(y)
+        y_rotated = dot_rz(l_max, phi)(y_rotated)
+        y_rotated = dot_rotation_matrix(l_max, None, None, R_mx)(y_rotated)
+        y_rotated = dot_rotation_matrix(l_max, None, None, R_obl)(y_rotated)
+        y_rotated = dot_rotation_matrix(l_max, None, None, R_inc)(y_rotated)
+        return y_rotated
+
+    def rot_flux(y, phi):
+        y = mp.matrix(y.tolist())
+        y_rotated = rotate_y(y, phi)
+        return ((_A1 @ y_rotated).T @ _rT)[0]
+
+    def occ_flux(y, b, r, phi, rotate=True):
+        y = mp.matrix(y.tolist())
+        xo = 0.0
+        yo = b
+        theta_z = mp.atan2(xo, yo)
+        _sT = get_sT(l_max, b, r)
+        x = _sT.T @ _A2
+        if rotate:
+            y_rotated = rotate_y(y, phi)
+            y_rotated = dot_rz(l_max, theta_z)(y_rotated)
+        else:
+            y_rotated = y
+        py = (_A1 @ y_rotated).T
+        return py @ x.T
+
+    def impl(y, b, r, phi, rotate=True):
+        if abs(b) >= (r + 1):
+            return rot_flux(y, phi)
+        elif r > 1 and abs(b) <= (r - 1):
+            return mp.mpf(0.0)
+        else:
+            return occ_flux(y, b, r, phi, rotate=rotate)[0]
+
+    return impl
