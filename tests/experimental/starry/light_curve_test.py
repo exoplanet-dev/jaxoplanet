40e033e969d181fec58097e2b5157bfab2c89ae2
Lionel Garcia <lionel_garcia@live.fr>
2024-09-16 10:38:06 -0400

    test: move multi-precision to general tests + supersede sympy symbolic (#218)
    * test: move multi-precision to general tests + supersede sympy symbolic
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix: move import in multiprecision tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * test: add test R multi-precision
    
    * fix: move mpmath import to skipped test
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix: missing import
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix: move import within skipped function
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/experimental/starry/light_curve_test.py
+++ tests/experimental/starry/light_curve_test.py
@@ -44,6 +44,56 @@ def test_compare_starry_limb_dark(deg, u):
     assert_allclose(calc, expected)
 
 
+@pytest.mark.parametrize("r", [0.01, 0.1, 1.0, 10.0, 100.0])
+def test_flux(r, l_max=5, order=500):
+    pytest.importorskip("mpmath")
+    from jaxoplanet.experimental.starry.multiprecision import flux as mp_flux, mp
+
+    # We know that these are were the errors are the highest
+    b = 1 - r if r < 1 else r
+
+    n = (l_max + 1) ** 2
+    expect = np.zeros(n)
+    calc = np.zeros(n)
+    ys = np.eye(n, dtype=np.float64)
+    ys[:, 0] = 1.0
+
+    @jax.jit
+    def light_curve(y):
+        surface = Surface(y=Ylm.from_dense(y, normalize=False), normalize=False)
+        return surface_light_curve(surface, y=b, z=10.0, r=r, order=order)
+
+    for i, y in enumerate(ys):
+        expect[i] = float(mp_flux.flux_function(l_max, mp.pi / 2, 0.0)(y, b, r, 0.0))
+        calc[i] = light_curve(y)
+
+    for n in range(expect.size):
+        # For logging/debugging purposes, work out the case id
+        l = np.floor(np.sqrt(n)).astype(int)  # noqa
+        m = n - l**2 - l
+        mu = l - m
+        nu = l + m
+        if mu == 1 and l == 1:
+            case = 1
+        elif mu % 2 == 0 and (mu // 2) % 2 == 0:
+            case = 2
+        elif mu == 1 and l % 2 == 0:
+            case = 3
+        elif mu == 1:
+            case = 4
+        elif (mu - 1) % 2 == 0 and ((mu - 1) // 2) % 2 == 0:
+            case = 5
+        else:
+            case = 0
+
+        assert_allclose(
+            calc[n],
+            expect[n],
+            err_msg=f"n={n}, l={l}, m={m}, mu={mu}, nu={nu}, case={case}",
+            atol=1e-6,
+        )
+
+
 @pytest.fixture(
     params=[
         {
--- tests/experimental/starry/solution_test.py
+++ tests/experimental/starry/solution_test.py
@@ -38,6 +38,47 @@ def test_kappas():
     assert_allclose(kappa1, 0.5 * np.pi - lam)
 
 
+@pytest.mark.parametrize("r", [0.01, 0.1, 1.0, 10.0, 100.0])
+def test_solution(r, l_max=5, order=500):
+    pytest.importorskip("mpmath")
+    from jaxoplanet.experimental.starry.multiprecision import (
+        solution as mp_solution,
+        utils,
+    )
+
+    # We know that these are were the errors are the highest
+    b = 1 - r if r < 1 else r
+
+    expect = utils.to_numpy(mp_solution.sT(l_max, b, r))
+    calc = solution_vector(l_max, order=order)(b, r)
+
+    for n in range(expect.size):
+        # For logging/debugging purposes, work out the case id
+        l = np.floor(np.sqrt(n)).astype(int)  # noqa
+        m = n - l**2 - l
+        mu = l - m
+        nu = l + m
+        if mu == 1 and l == 1:
+            case = 1
+        elif mu % 2 == 0 and (mu // 2) % 2 == 0:
+            case = 2
+        elif mu == 1 and l % 2 == 0:
+            case = 3
+        elif mu == 1:
+            case = 4
+        elif (mu - 1) % 2 == 0 and ((mu - 1) // 2) % 2 == 0:
+            case = 5
+        else:
+            case = 0
+
+        assert_allclose(
+            calc[n],
+            expect[n],
+            err_msg=f"n={n}, l={l}, m={m}, mu={mu}, nu={nu}, case={case}",
+            atol=1e-15,
+        )
+
+
 @pytest.mark.parametrize("r", [0.1, 1.1])
 def test_solution_compare_starry(r, l_max=10, order=20):
     starry = pytest.importorskip("starry")
