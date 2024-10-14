8d328ad08396ae0cd53c15a06c1b03f4399037e0
lgrcia <lionel_garcia@live.fr>
2024-10-14 16:50:07 -0400

    fix: typo

--- src/jaxoplanet/experimental/starry/multiprecision/precision.py
+++ src/jaxoplanet/experimental/starry/multiprecision/precision.py
@@ -25,7 +25,7 @@ def starry_light_curve(
     u = [1.0] * udeg
     y = np.ones((ydeg + 1) ** 2)
     surface = Surface(y=Ylm.from_dense(y), u=u, normalize=False)
-    expected = flux.flux.flux(ydeg=ydeg, udeg=udeg)(b, r, u=u, y=y)
+    expected = flux.flux(ydeg=ydeg, udeg=udeg)(b, r, u=u, y=y)
     calc = surface_light_curve(surface, y=b, z=1.0, r=r, order=order)
     return float(np.abs(float(mp.mpf(float(calc)) - expected)))
 
@@ -47,6 +47,6 @@ def limb_dark_light_curve(
     if b is None:
         b = 1 - r if r < 1.0 else r
     u = [1.0] * udeg
-    expected = flux.flux.flux(udeg=udeg)(b, r, u=u)
+    expected = flux.flux(udeg=udeg)(b, r, u=u)
     calc = 1 + ld_light_curve(u, b, r, order=order)
     return np.abs(float(mp.mpf(float(calc)) - expected))
