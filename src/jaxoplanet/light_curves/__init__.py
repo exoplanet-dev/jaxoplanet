3065d9c6389de65ca3b4a040ea28e94c04cbfe77
lgrcia <lionel_garcia@live.fr>
2025-04-01 12:45:08 -0400

    fix: add power_two_light_curve to _all__

--- src/jaxoplanet/light_curves/__init__.py
+++ src/jaxoplanet/light_curves/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ["transforms", "limb_dark_light_curve"]
+__all__ = ["transforms", "limb_dark_light_curve", "power_two_light_curve"]
 
 from jaxoplanet.light_curves import transforms as transforms
 from jaxoplanet.light_curves.limb_dark import light_curve as limb_dark_light_curve
