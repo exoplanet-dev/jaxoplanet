dc53a19724c2c367e6f5490f9f34c6f3de8b4ec6
Soichiro Hattori <soichiro.hattori@gmail.com>
2025-04-21 00:56:37 -0400

    cherry-picking

--- src/jaxoplanet/__init__.py
+++ src/jaxoplanet/__init__.py
@@ -1,9 +1,10 @@
-__all__ = ["core", "light_curves", "orbits", "units"]
+__all__ = ["core", "light_curves", "orbits", "units", "experimental"]
 
 from jaxoplanet import (
     core as core,
     light_curves as light_curves,
     orbits as orbits,
     units as units,
+    experimental as experimental,
 )
 from jaxoplanet.jaxoplanet_version import __version__ as __version__
